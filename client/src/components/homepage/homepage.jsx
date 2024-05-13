54f17a6328dec2b751609b72039667a8e2c3d027
Galaxieed <charleslpillos@gmail.com>
2024-05-13 16:09:09 +0800

    removed margin padding in body

--- client/src/components/homepage/homepage.jsx
+++ client/src/components/homepage/homepage.jsx
@@ -1,6 +1,7 @@
 export default function Homepage() {
     return (
         <>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae nemo delectus nihil repudiandae ex, aspernatur modi ad exercitationem quas dolor, quam dolores sed. Aperiam explicabo dolores quis quidem, praesentium reiciendis.</p>
         </>
     )
 }
